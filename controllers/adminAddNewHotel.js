780f2378e15076641be007c9aef2620ab0183bd6
Khalid Hasan <38977344+khalid-hasan@users.noreply.github.com>
2019-03-09 22:26:35 +0600

    Message Added

--- controllers/adminAddNewHotel.js
+++ controllers/adminAddNewHotel.js
@@ -26,7 +26,8 @@ router.get('/', (req, res)=>{
     userModel.getUsernames(function(results) {
 
             var user = {
-            list: results
+            list: results,
+            message: ''
             };
 
             res.render('admin/newHotel', user);
@@ -41,13 +42,15 @@ router.post('/', (req, res)=>{
         hotel_desc : req.body.hotel_desc,
         hotel_owner : req.body.hotel_owner,
         image : 'dist/img/hotel-1-1.jpg',
-        addedBy : 'Khalid'
+        addedBy : 'Khalid',
+        message : 'Hotel Added',
+        list : ''
     };
 
     userModel.insert(user, function(success){
 		if(success){
             console.log(user);
-			res.redirect('http://google.com');
+			res.render('admin/newHotel', user);
 		}else{
 			res.send("Failed");
 		}
